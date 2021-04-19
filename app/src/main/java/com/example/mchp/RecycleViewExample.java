c2480eda5d0a97785dfb3208237514d5401df608
dsizo <dasizov@edu.hse.ru>
2021-04-19 11:16:55 +0300

    WebViewExample & Retrofit start

--- app/src/main/java/com/example/mchp/RecycleViewExample.java
+++ app/src/main/java/com/example/mchp/RecycleViewExample.java
@@ -7,6 +7,7 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
